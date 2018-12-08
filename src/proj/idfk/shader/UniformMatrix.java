d25b397753d97a7dd63887d6d7c9d2553f30faf6
DidntRead <gabro2003@gmail.com>
2018-12-08 19:49:27 +0200

    Add shader package

---
+++ src/proj/idfk/shader/UniformMatrix.java
@@ -0,0 +1,24 @@
+package proj.idfk.shader;
+
+import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL45;
+
+import java.nio.FloatBuffer;
+
+public class UniformMatrix extends Uniform{
+    private static FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
+
+    public UniformMatrix(String name) {
+        super(name);
+    }
+
+    public void loadMatrix(Matrix4f mat) {
+        mat.get(matrixBuffer);
+        GL45.glProgramUniformMatrix4fv(programID, location, false, matrixBuffer);
+    }
+
+    public void loadMatrix(FloatBuffer mat) {
+        GL45.glProgramUniformMatrix4fv(programID, location, false, mat);
+    }
+}
