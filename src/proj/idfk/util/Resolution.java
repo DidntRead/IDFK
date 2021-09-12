4af2de60016b6867174a1f126aebbb5e401018b8
DidntRead <25854753+DidntRead@users.noreply.github.com>
2021-09-12 13:33:46 +0300

    util: add all uncommited utils

--- src/proj/idfk/util/Resolution.java
+++ src/proj/idfk/util/Resolution.java
@@ -1,12 +1,25 @@
 package proj.idfk.util;
 
-public enum Resolution {
-    RES800x600(800, 600), RES1280x1024(1280, 1024), RES1366x768(1366, 768), RES1600x900(1600, 900), RES1920x1080(1920, 1080), RES2560x1440(2560, 1440), RES3840x2160(3840, 2160);
-
+public class Resolution {
     final public int width, height;
-    Resolution(int width, int height) {
+
+    public Resolution(int width, int height) {
         this.width = width;
         this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(Resolution.class.isAssignableFrom(obj.getClass()))) {
+            return false;
+        }
+
+        Resolution res = (Resolution) obj;
+
+        return res.width == this.width && res.height == this.height;
     }
 
     @Override
